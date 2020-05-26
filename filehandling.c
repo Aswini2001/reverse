5d9d75fef79ed7eca3b4e7718dcc4c2b58d42f1b
Aswini2001 <64783778+Aswini2001@users.noreply.github.com>
2020-05-26 22:28:46 +0530

    created file handling.c
    the output will be the reverse content of a file.

---
+++ filehandling.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	FILE *fp,*fp2;
+	
+	int i,pos;
+	int ch;
+    fp = fopen("F:\mycap.txt","r");
+   	fp2 =fopen("F:\out.txt","w");
+		
+	while((ch=fgetc (fp))!=EOF)
+{
+	fputc(ch,fp2);
+
+	} 	
+	fseek(fp,0,SEEK_END);
+	pos = ftell(fp);
+	i=0;
+	while(i<pos)
+	{
+		i++;
+		fseek(fp,-i,SEEK_END);
+		printf("%c",fgetc(fp));
+		
+	}
+
+}
